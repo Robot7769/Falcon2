2af6f3d2cd707e58d1209157d2de2ffc639d8fdd
Robot7769 <jan.robot7@gmail.com>
2019-06-01 04:45:25 +0200

    funkční program 1.6.2019 4:44 "Black Falcon"

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,11 @@
 // #include <HardwareSerial.h>
 #include <ODriveArduino.h>
 #define PNR 57.29577951308232
+#define SERVOMAX 180
+#define SERVOMIN 0
+#define AXIS_COUNT 8
+#define BTN_COUNT 11
+
 
 using rb::LED_GREEN;
 void configureOdrive( ODriveArduino& odrive );
@@ -30,30 +35,17 @@ int position_servo0 = 85;
 int position_servo1 = 165;
 int position_servo2 = 180;
 int position_servo3 = 160;
-int alfa = 60;
-double alfa_rad = 1.04719755119659;
-int beta = 30;
-double beta_rad = 0.52359877559829;
-int gama = 90;
-double gama_rad = 1.57079632679489;
-float arm1 = 120.00;         //v mm
-float arm2 = 80.00;          //v mm
-float prepona_arm = 143.00;  //v mm
-float delka_arm = 70.00;     //v mm
-float vyska_arm = 120.00;    //v mm
-float max_arm = arm1 + arm2;
-float min_arm = 0.00;
-float krok_arm = 3.00;
-int krok_serva = 2;
-int motor_power = 80;
+float krok_arm = 1.20;
+int krok_serva = 3;
+int motor_power = 99;
 bool L_G_light = false; // pro blikani zelene LED - indikuje, ze deska funguje
 int otocka_kola = 13 * 2400 ; // převodovka (1:5) 1:8,  2400 tiků enkodéru na otáčku motoru
 long max_speed = 20000; // pocet tiku za sekundu max cca 200000,  enkodéry zvládají cca 5000 otacek motoru za sekundu
-int speed_coef = 200000; // nasobeni hodnoty, co leze z joysticku
+int speed_coef = 200; // nasobeni hodnoty, co leze z joysticku
 
-int axis[7] = {5,6,7,8,9,10,11};
-byte btn[8] = {0,0,0,0,0,0,0,0};
-byte btn_last[8] = {0,0,0,0,0,0,0,0};
+int axis[AXIS_COUNT] = {5,6,7,8,9,10,11};
+byte btn[BTN_COUNT] = {0,0,0,0,0,0,0,0};
+byte btn_changed[BTN_COUNT] = {0,0,0,0,0,0,0,0};
 
 BluetoothSerial SerialBT;
 Stream* serial = nullptr;
@@ -88,8 +80,10 @@ void setup() {
     servo3.attach(32);
     servo3.write(position_servo3);
 
- 
-    rbc().leds().blue( true );  // zapne modrou LED - tim zapne i Odrive 
+
+    rbc().leds().blue( true );  // zapne modrou LED - tim zapne i Odrive
+    pinMode(GPIO_NUM_21,OUTPUT );
+    digitalWrite(GPIO_NUM_21, HIGH);
 
     odriveSerial.begin(115200, SERIAL_8N1, 13, 15);
     Serial.println( "Setup odrive begin" );
@@ -115,12 +109,12 @@ void setup() {
     Serial.println( "Turned on" );
     delay( 500 );
 
-        odrive.move( 0, 0, max_speed ); // dojeď s osou 0 na pozici 0 rychlostí max_speed tiků na otáčku 
-        odrive.move( 1, 0, max_speed ); 
+        odrive.move( 0, 0, max_speed ); // dojeď s osou 0 na pozici 0 rychlostí max_speed tiků na otáčku
+        odrive.move( 1, 0, max_speed );
         delay( 4000 );
 
         odrive.move( 0, otocka_kola / 2, max_speed );
-        odrive.move( 1, otocka_kola / 2, max_speed ); 
+        odrive.move( 1, otocka_kola / 2, max_speed );
         delay( 1000 );
 
         odrive.move( 0, 0, max_speed );
@@ -145,6 +139,7 @@ void setup() {
 
     // delay(500);
        send_data.restart();
+
 }
 void arm();
 void testovaci(); // dole pod main
@@ -156,85 +151,106 @@ void loop() {
         send_data.ack();
         if (L_G_light) L_G_light = false; else  L_G_light = true;
         rbc().leds().green(L_G_light);
-        Serial.println( millis() );
+        //Serial.println( millis() );
         SerialBT.println( millis() ); // na pocitaci Burda COM port 13
     }
 
-    // arm();
-    testovaci();
-    // if ( read_joystick() )
-    // {
-    //     float axis_0 = (abs(axis[0]) < 10) ? 0 : axis[0] /128.0; 
-    //     axis_0 = axis_0*axis_0*axis_0;
-    //     float axis_1 = (abs(axis[1]) < 10) ? 0 : axis[1] /128.0; 
-    //     axis_1 = axis_1*axis_1*axis_1;
-    //     int levy_m = -(axis_1- (axis_0 /2 )) * speed_coef;
-    //     int pravy_m = -(axis_1+ (axis_0 /2 )) * speed_coef;
-    //     odrive.speed( 0 , levy_m );
-    //     odrive.speed( 1 , pravy_m  );
-    //     if ( odrive.error() )
-    //         odrive.dumpErrors();
-    //     printf(" %i %i \n ", levy_m, pravy_m );
-    //     Serial.println(levy_m);
-    //     Serial.println(pravy_m);
-    //     SerialBT.println(levy_m);
-    //     SerialBT.println(pravy_m);
-    //     SerialBT.print(levy_m); SerialBT.print(" "); SerialBT.println(pravy_m);
-    // }
+    arm();
+    //testovaci();
+    if ( read_joystick() )
+    {
+        // float axis_0 = (abs(axis[0]) < 10) ? 0 : axis[0] /128.0;
+        // axis_0 = axis_0*axis_0*axis_0;
+        // float axis_1 = (abs(axis[1]) < 10) ? 0 : axis[1] /128.0;
+        // axis_1 = axis_1*axis_1*axis_1;
+        // int levy_m = -(axis_1- (axis_0 /2 )) * speed_coef;
+        // int pravy_m = -(axis_1+ (axis_0 /2 )) * speed_coef;
+        float axis_0 = (abs(axis[0]) < 15) ? 0 : axis[0];
+        float axis_1 = (abs(axis[2]) < 15) ? 0 : axis[2];
+        float levy_m = (-axis_1- (axis_0 /2 )) * speed_coef;
+        float pravy_m = (-axis_1+ (axis_0 /2 )) * speed_coef;
+        odrive.speed( 0 , levy_m );
+        odrive.speed( 1 , pravy_m  );
+        if ( odrive.error() )
+            odrive.dumpErrors();
+        printf(" %f %f \n ", levy_m, pravy_m );
+        Serial.println(levy_m);
+        Serial.println(pravy_m);
+        SerialBT.println(levy_m);
+        SerialBT.println(pravy_m);
+        SerialBT.print(levy_m); SerialBT.print(" "); SerialBT.println(pravy_m);
+    }
     delay(10);
-} 
+}
 
 
 // ********************************************************************
 
 bool read_joystick()
 {
-    if ( SerialBT.available() == 0 )
-        return false;
+  if ( SerialBT.available() == 0 )
+          return false;
 
-    int test = SerialBT.read();
-    if (test == 0x80)
-    {
-        int axis_count = SerialBT.read();
-        for (int x = 0; x < axis_count; x++)
-        {
-            while(SerialBT.available() < 1)
-            {
-                // DO NOTHING - WAITING FOR PACKET
-                delay(1);
-            }
+      int test = SerialBT.read();
+      if (test == 0x80)
+      {
+          int axis_count = SerialBT.read();
+          if (axis_count >= AXIS_COUNT)
+          {
+              Serial.println("********* CHYBA V POCTU OS !!! ************");
+          }
+          else
+          {
+              for (int x = 0; x < axis_count; x++)
+              {
+                  while(SerialBT.available() < 1)
+                  {
+                      // DO NOTHING - WAITING FOR PACKET
+                      delay(1);
+                  }
 
-            int8_t tmp = SerialBT.read();
-            axis[x] = tmp;
-            Serial.print(x);  
-            Serial.print(": ");
-            Serial.print(axis[x], DEC);
-            Serial.print(" ");
-            SerialBT.print(x);
-            SerialBT.print(": ");
-            SerialBT.print(axis[x], DEC);
-            SerialBT.print(" ");
+                  int8_t tmp = SerialBT.read();
+                  axis[x] = tmp;
+                  Serial.print(x);
+                  Serial.print(": ");
+                  Serial.print(axis[x], DEC);
+                  Serial.print(" ");
+                  SerialBT.print(x);
+                  SerialBT.print(": ");
+                  SerialBT.print(axis[x], DEC);
+                  SerialBT.print(" ");
 
-        }
-        return true;
-    }
-    else if  ( test == 0x81 )
-    {
-        while(SerialBT.available() < 1) {
-            // DO NOTHING - WAITING FOR PACKET
-            delay(1);
-        }
-        byte a = SerialBT.read();
-        while(SerialBT.available() < 1) {
-            // DO NOTHING - WAITING FOR PACKET
-            delay(1);
-        }
-        btn_last[a] = btn[a];
-        btn[a] = SerialBT.read();
-        Serial.print(a, DEC); Serial.print(": "); Serial.print(btn[a], DEC); Serial.print("last: "); Serial.print(btn_last[a], DEC);
-        return true;
-    }
-    return false;
+              }
+              return true;
+          }
+
+
+      }
+      else if  ( test == 0x81 )
+      {
+          while(SerialBT.available() < 1) {
+              // DO NOTHING - WAITING FOR PACKET
+              delay(1);
+          }
+          byte a = SerialBT.read();
+          if ( a >= BTN_COUNT )
+          {
+              Serial.println("********* CHYBA V POCTU TLACITEK !!! ************");
+          }
+          else
+          {
+              while(SerialBT.available() < 1) {
+                  // DO NOTHING - WAITING FOR PACKET
+                  delay(1);
+              }
+              btn_changed[a] = 1;
+              btn[a] = SerialBT.read();
+              Serial.print(a, DEC); Serial.print(": "); Serial.print(btn[a], DEC); Serial.print("changed: "); Serial.print(btn_changed[a], DEC);
+              return true;
+          }
+
+      }
+      return false;
 }
 
 
@@ -244,7 +260,7 @@ void testovaci()
    char c;
     if(Serial.available() or SerialBT.available() )  {
         if(Serial.available()) c = Serial.read(); else c = SerialBT.read();
-        Serial.write("c: "); Serial.println(c); // Serial.write(" "); Serial.println(c,DEC);
+        //Serial.write("c: "); Serial.println(c); // Serial.write(" "); Serial.println(c,DEC);
             switch(c) {
                 case 'q':
                     if (position_servo3 >= 5)  position_servo3 = position_servo3 - krok_serva;
@@ -271,6 +287,7 @@ void testovaci()
                     Serial.print(position_servo2);
                     break;
                 case 'y':
+
                     if (position_servo1 >= 5)  position_servo1 = position_servo1 - krok_serva;
                     servo1.write(position_servo1);
                     Serial.write(" 1: ");
@@ -312,7 +329,7 @@ void testovaci()
                     Serial.write(c);
                     break;
             }
-        
+
         // delay(10);
         // Serial.println(millis());
 
@@ -324,57 +341,69 @@ void testovaci()
 
 void arm()
 {
-    if (axis[6]<-10)
-    {
-        if (vyska_arm < max_arm)
-        {
-          vyska_arm = vyska_arm + krok_arm;
-        }
-    }
-    else if (axis[6]> 10)
-    {
-        if (vyska_arm > min_arm)
-        {
-          vyska_arm = vyska_arm - krok_arm;
-        }
-    }
 
-    if (axis[3]<-10)
-    {
-        if (delka_arm < max_arm)
-        {
-          delka_arm = delka_arm + krok_arm;
-        }
+
+
+    if (btn[3]==1) {
+      if((position_servo3 < SERVOMAX) and(position_servo3 > SERVOMIN)){
+        position_servo2++;
+      }
     }
-    else if (axis[3]> 10)
-    {
-        if (delka_arm > min_arm)
-        {
-          delka_arm = delka_arm - krok_arm;
-        }
+    else if (btn[0]==1) {
+      if((position_servo3 < SERVOMAX) and(position_servo3 > SERVOMIN)){
+        position_servo2--;
+      }
     }
 
-    prepona_arm = sqrt(((delka_arm*delka_arm)+(vyska_arm*vyska_arm)));
-    alfa_rad = atan((vyska_arm/delka_arm));
-    alfa = (int)round((alfa_rad*PNR));
-    beta_rad = acos((((arm1*arm1)+(prepona_arm*prepona_arm)-(arm2*arm2))/(2*arm1*prepona_arm)));
-    beta = (int)round((beta_rad*PNR));
-    gama_rad = acos((((arm2*arm2)+(arm1*arm1)-(prepona_arm*prepona_arm))/(2*arm2*arm1)));
-    gama = (int)round((gama_rad*PNR));
+    if (btn[2]==1) {
+      if((position_servo3 < SERVOMAX) and (position_servo3 > SERVOMIN)){
+        position_servo3--;
+      }
+    }
+    else if (btn[1]==1) {
+      if((position_servo3 < SERVOMAX) and (position_servo3 > SERVOMIN)){
+        position_servo3++;
+      }
+    }
 
-    position_servo0 = 180 - alfa - beta;
-    position_servo1 = gama;
+    if (axis[6]<-15) {
+      position_servo0++;
+    }
+    else if (axis[6]>15) {
+      position_servo0--;
+    }
 
-    if ( (btn[4]==1) and (btn_last[4]==0) )
+    if (axis[3]<-15) {
+      position_servo1++;
+    }
+    else if (axis[3]>15) {
+      position_servo1--;
+    }
+
+    if (btn_changed[4]) {
+      if (btn[4]) {
         rbc().setMotors().power(OTOCNY_MOTOR, -motor_power)
-                             .set();
-    if ( (btn[4]==0) and (btn_last[4]==1) )
+                         .set();
+      } else {
         rbc().setMotors().power(OTOCNY_MOTOR, 0)
                          .set();
-    if ( (btn[5]==1) and (btn_last[5]==0) )
+      }
+      btn_changed[6] = 0;
+    }
+    if (btn_changed[5]) {
+      if (btn[5]) {
         rbc().setMotors().power(OTOCNY_MOTOR, motor_power)
                          .set();
-    if ( (btn[5]==0) and (btn_last[5]==1) )
+      } else {
         rbc().setMotors().power(OTOCNY_MOTOR, 0)
                          .set();
+      }
+      btn_changed[5] = 0;
+    }
+
+    servo0.write(position_servo0);
+    servo1.write(position_servo1);
+    servo2.write(position_servo2);
+    servo3.write(position_servo3);
+
 }
